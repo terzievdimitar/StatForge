06e5db2d971277ace134902635eefc30e41e38f1
Marin <marinenikolov@gmail.com>
2025-11-15 13:35:32 +0100

    Pricing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,61 +1,57 @@
-import { Routes, Route } from 'react-router-dom';
-import { useEffect } from 'react';
-import LandingPage from './pages/LandingPage.tsx';
-import Navbar from './components/Navbar.tsx';
-import Signup from './pages/Signup.tsx';
-import Login from './pages/Login.tsx';
-import HostingPage from './pages/HostingPage.tsx';
-import { useUserStore } from './stores/useUserStore.ts';
-import LoadingSpinner from './components/LoadingSpinner.tsx';
-import { useGithubStore } from './stores/useGithubStore.ts';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme/theme.ts';
-import AboutPage from './pages/AboutPage.tsx';
+import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import LandingPage from "./pages/LandingPage.tsx";
+import Navbar from "./components/Navbar.tsx";
+import Signup from "./pages/Signup.tsx";
+import Login from "./pages/Login.tsx";
+import HostingPage from "./pages/HostingPage.tsx";
+import { useUserStore } from "./stores/useUserStore.ts";
+import LoadingSpinner from "./components/LoadingSpinner.tsx";
+import { useGithubStore } from "./stores/useGithubStore.ts";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme/theme.ts";
+import AboutPage from "./pages/AboutPage.tsx";
+import PricingPage from "./pages/PricingPage.tsx";
 
 function App() {
-	const { user, checkAuth, checkingAuth } = useUserStore();
-	const { getRepositories } = useGithubStore();
+  const { user, checkAuth, checkingAuth } = useUserStore();
+  const { getRepositories } = useGithubStore();
 
-	// Check authentication status on app load
-	useEffect(() => {
-		checkAuth();
-	}, [checkAuth]);
+  // Check authentication status on app load
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
 
-	useEffect(() => {
-		if (!user) return;
-		getRepositories();
-	}, [getRepositories, user]);
+  useEffect(() => {
+    if (!user) return;
+    getRepositories();
+  }, [getRepositories, user]);
 
-	if (checkingAuth) {
-		return <LoadingSpinner />;
-	}
-	return (
-		<ThemeProvider theme={theme}>
-			<Navbar />
-			<Routes>
-				<Route
-					path='/'
-					element={!user ? <LandingPage /> : <HostingPage />}
-				/>
-				<Route
-					path='/signup'
-					element={!user ? <Signup /> : <LandingPage />}
-				/>
-				<Route
-					path='/login'
-					element={!user ? <Login /> : <LandingPage />}
-				/>
-				<Route
-					path='/about'
-					element={!user ? <AboutPage /> : <HostingPage />}
-				/>
-				<Route
-					path='/dashboard/hosting'
-					element={user ? <HostingPage /> : <Login />}
-				/>
-			</Routes>
-		</ThemeProvider>
-	);
+  if (checkingAuth) {
+    return <LoadingSpinner />;
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={!user ? <LandingPage /> : <HostingPage />} />
+        <Route
+          path="/pricing"
+          element={!user ? <PricingPage /> : <LandingPage />}
+        />
+        <Route path="/signup" element={!user ? <Signup /> : <LandingPage />} />
+        <Route path="/login" element={!user ? <Login /> : <LandingPage />} />
+        <Route
+          path="/about"
+          element={!user ? <AboutPage /> : <HostingPage />}
+        />
+        <Route
+          path="/dashboard/hosting"
+          element={user ? <HostingPage /> : <Login />}
+        />
+      </Routes>
+    </ThemeProvider>
+  );
 }
 
 export default App;
