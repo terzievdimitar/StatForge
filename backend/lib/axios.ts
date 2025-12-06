3b0720ed07af52e7b1736bf35a1019c93ffa54ea
Plo4i <midag@abv.bg>
2025-12-06 19:48:07 +0100

    fix: update axios baseURL to use production URL for deployment

--- backend/lib/axios.ts
+++ backend/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/api' : '/api';
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://statforge-ro5u.onrender.com/api' : 'http://localhost:3000/api';
 
 const axiosInstance = axios.create({
 	baseURL: baseURL,
